b446c9a41c701304e3de4e770082c4cbeb1de5ea
Andreas Oen <andreas.oentoro@bukalapak.com>
2019-06-22 10:50:00 +0700

    Add item feed layout

--- app/src/main/java/com/mindorks/bootcamp/instagram/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/mindorks/bootcamp/instagram/ui/feed/FeedFragment.kt
@@ -12,7 +12,8 @@ import kotlinx.android.synthetic.main.toolbar.view.*
 class FeedFragment : BaseFragment<FeedViewModel>() {
     override fun provideLayoutId(): Int = R.layout.fragment_feed
 
-    override fun injectDependencies(fragmentComponent: FragmentComponent) = fragmentComponent.inject(this)
+    override fun injectDependencies(fragmentComponent: FragmentComponent) =
+            fragmentComponent.inject(this)
 
 
     override fun setupView(view: View) {
