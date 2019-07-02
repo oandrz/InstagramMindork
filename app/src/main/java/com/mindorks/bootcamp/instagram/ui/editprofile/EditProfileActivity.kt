606aa73e80d1e9ec646f69a8fb3cc52db5c253b5
Andreas Oen <andreas.oentoro@bukalapak.com>
2019-07-02 10:54:23 +0700

    Add action bar button

--- app/src/main/java/com/mindorks/bootcamp/instagram/ui/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/mindorks/bootcamp/instagram/ui/editprofile/EditProfileActivity.kt
@@ -3,9 +3,12 @@ package com.mindorks.bootcamp.instagram.ui.editprofile
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.mindorks.bootcamp.instagram.R
 import com.mindorks.bootcamp.instagram.di.component.ActivityComponent
 import com.mindorks.bootcamp.instagram.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.toolbar.*
 
 class EditProfileActivity : BaseActivity<EditProfileViewModel>() {
     override fun provideLayoutId(): Int = R.layout.activity_edit_profile
@@ -15,6 +18,27 @@ class EditProfileActivity : BaseActivity<EditProfileViewModel>() {
     }
 
     override fun setupView(savedInstanceState: Bundle?) {
+        toolbar.title = getString(R.string.editprofile_title)
+        setSupportActionBar(toolbar)
+        supportActionBar?.run {
+            setHomeButtonEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_cancel)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_edit_profile, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.menu_confirm ->
+                true
+            else ->
+                super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
